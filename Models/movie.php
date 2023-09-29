6e1c0b45e2ab0060f2f7b2d35cc531e1bab112df
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-09-29 14:51:56 +0200

    add genere.php

--- Models/movie.php
+++ Models/movie.php
@@ -2,24 +2,24 @@
 class Movie {
     public $titolo;
     public $anno;
-    public $genere;
     public $lingua;
+    public $genere;
 
     function __construct
     (string $titolo, 
     string $anno, 
-    string $genere, 
-    string $lingua 
+    string $lingua, 
+    Genere $genere, 
     )
     {
        $this->titolo = $titolo;
        $this->anno = $anno;
-       $this->genere = $genere;
        $this->lingua = $lingua;
+       $this->genere = $genere;
     }
 
     public function getDescriptionMovie(){
-        return "Titolo: $this->titolo <br> Anno: $this->anno <br> Genere: $this->genere <br> Lingua: $this->lingua";
+        return "Titolo: $this->titolo <br> Anno: $this->anno <br> Lingua: $this->lingua <br> Genere: " . $this->genere->getName() . "";
     }
 
 }
--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 require_once __DIR__ . './Models/movie.php';
 require_once __DIR__ . './Models/genere.php';
 
+$actionGenere = new Genere("Azione");
+$comedyGenere = new Genere("Commedia");
 
-$movie_1 = New Movie('Come un gatto in tangenziale','2017', 'Commedia', 'Italiana');
-$movie_2 = New Movie('John Wick','2014','Azione', 'Inglese');
+$movie_1 = New Movie('Come un gatto in tangenziale','2017','Italiana', $comedyGenere);
+$movie_2 = New Movie('John Wick','2014','Inglese', $actionGenere,);
 
 $movie_1 -> getDescriptionMovie();
 $movie_2 -> getDescriptionMovie();
