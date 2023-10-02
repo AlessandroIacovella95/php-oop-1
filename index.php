fe5ec851601da6c97347c0c01985a4de0c130857
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-10-02 15:09:52 +0200

    fix errors

--- index.php
+++ index.php
@@ -3,13 +3,12 @@
 require_once __DIR__ . './Models/movie.php';
 require_once __DIR__ . './Models/serieTv.php';
 require_once __DIR__ . './Models/genere.php';
-require_once __DIR__ . './production.php';
-
 
 $comedyGenere = new Genere("Commedia");
+$horrorGenere = New Genere ("Horror");
 $detectiveStoryGenere = new Genere("Poliziesco");
 
-$production = New Production ('titolo', 'italiano', $comedyGenere );
+$production = New Production ('titolo', 'italiano', $horrorGenere );
 
 $movie_1 = New Movie('Come un gatto in tangenziale','Italiana', $comedyGenere, '2017', 90 );
 $serie_1 = New TvSerie('Criminal Minds','Inglese', $detectiveStoryGenere, '2005', '2016', 334, 16);
